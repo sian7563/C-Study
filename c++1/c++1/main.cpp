965b41510cec480064b0b60d60ee4e27f1a023f1
sian7563 <sian7563@naver.com>
2021-08-23 22:03:12 +0900

    8/23 c++

--- c++1/c++1/main.cpp
+++ c++1/c++1/main.cpp
@@ -34,14 +34,23 @@ int main()
 //    std::cout << "b2 : " << b2 << std::endl;
 //    std::cout << std::endl;
     
-    std::cout << "bool과 if" << std::endl;
-    if (b0)
-        std::cout << "b0" << std::endl;
-    if (b1)
-        std::cout << "b1" << std::endl;
-    if (b2)
-        std::cout << "b2" << std::endl;
+//    std::cout << "bool과 if" << std::endl;
+//    if (b0)
+//        std::cout << "b0" << std::endl;
+//    if (b1)
+//        std::cout << "b1" << std::endl;
+//    if (b2)
+//        std::cout << "b2" << std::endl;
+//    std::cout << std::endl;
+    
+    std::cout << "bool 에서 정수로 형 변환" << std::endl;
+    int itrue = true;
+    int ifalse = false;
+    
+    std::cout << itrue << std::endl;
+    std::cout << ifalse << std::endl;
     std::cout << std::endl;
+
     
     return 0;
 }
