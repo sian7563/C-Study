187b12b17248895cb2594bffafe5f9a99f990410
sian7563 <sian7563@naver.com>
2021-08-23 21:59:43 +0900

    8/23 c++

--- c++1/c++1/main.cpp
+++ c++1/c++1/main.cpp
@@ -9,12 +9,23 @@
 
 int main()
 {
-    std::cout << "******" << std::endl;
-    std::cout << "******" << std::endl;
-    std::cout << "******" << std::endl;
-    std::cout << "******" << std::endl;
-    std::cout << "******" << std::endl;
-    std::cout << "******" << std::endl;
+    
+//    std::cout << "******" << std::endl;
+//    std::cout << "******" << std::endl;
+//    std::cout << "******" << std::endl;
+//    std::cout << "******" << std::endl;
+//    std::cout << "******" << std::endl;
+//    std::cout << "******" << std::endl;
+
+    bool b0 = 0 == 0;
+    bool b1 = 0 < 1;
+    bool b2 = 0 > 1;
+
+    std::cout << "bool 출력" << std::endl;
+    std::cout << "b0 : " << b0 << std::endl;
+    std::cout << "b1 : " << b1 << std::endl;
+    std::cout << "b2 : " << b2 << std::endl;
+    std::cout << std::endl;
 
     return 0;
 }
