581fd13140d9f561d1783fe711b82b0d04885fde
sian7563 <sian7563@naver.com>
2021-08-23 22:00:53 +0900

    8/23 c++

--- c++1/c++1/main.cpp
+++ c++1/c++1/main.cpp
@@ -21,12 +21,19 @@ int main()
     bool b1 = 0 < 1;
     bool b2 = 0 > 1;
 
-    std::cout << "bool 출력" << std::endl;
+//    std::cout << "bool 출력" << std::endl;
+//    std::cout << "b0 : " << b0 << std::endl;
+//    std::cout << "b1 : " << b1 << std::endl;
+//    std::cout << "b2 : " << b2 << std::endl;
+//    std::cout << std::endl;
+
+    std::cout << "bool 출력을 true, false로 하도록 변경" << std::endl;
+    std::cout.setf(std::ios_base::boolalpha);
     std::cout << "b0 : " << b0 << std::endl;
     std::cout << "b1 : " << b1 << std::endl;
     std::cout << "b2 : " << b2 << std::endl;
     std::cout << std::endl;
-
+    
     return 0;
 }
 
