8062d0c3c059278ac3396a8f9efb10ddb341d918
sian7563 <sian7563@naver.com>
2021-08-23 21:57:59 +0900

    8/23 c++

---
+++ c++1/c++1/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  c++1
+//
+//  Created by kimsian on 2021/08/23.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "******" << std::endl;
+    std::cout << "******" << std::endl;
+    std::cout << "******" << std::endl;
+    std::cout << "******" << std::endl;
+    std::cout << "******" << std::endl;
+    std::cout << "******" << std::endl;
+
+    return 0;
+}
+
