b23c8c72bbd1168b4593e85315fd8f983b70512f
sian7563 <sian7563@naver.com>
2021-08-25 08:53:31 +0900

    변수 선언 및 정의

---
+++ c++2/c++2/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  c++2
+//
+//  Created by kimsian on 2021/08/25.
+//
+
+#include <iostream>
+
+int main()
+{
+    
+    int num = 10;
+    int num2(20);
+    int num3(num2);
+
+    std::cout << "num : " << num << std::endl;
+    std::cout << "num2 : " << num2 << std::endl;
+    std::cout << "num3 : " << num3 << std::endl;
+
+    return 0;
+}
