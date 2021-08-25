a284a31b2205ed7c92617c0a23fbe20b8eae741e
sian7563 <sian7563@naver.com>
2021-08-25 09:00:23 +0900

    auto 예약어

--- c++2/c++2/main.cpp
+++ c++2/c++2/main.cpp
@@ -6,17 +6,32 @@
 //
 
 #include <iostream>
+#include <typeinfo>
 
 int main()
 {
     
-    int num = 10;
-    int num2(20);
-    int num3(num2);
+//    int num = 10;
+//    int num2(20);
+//    int num3(num2);
+//
+//    std::cout << "num : " << num << std::endl;
+//    std::cout << "num2 : " << num2 << std::endl;
+//    std::cout << "num3 : " << num3 << std::endl;
 
-    std::cout << "num : " << num << std::endl;
-    std::cout << "num2 : " << num2 << std::endl;
-    std::cout << "num3 : " << num3 << std::endl;
+    auto nData = 10;
+    std::cout << "nData : " << nData << ", "
+              << typeid(nData).name() << std::endl;
+
+    auto strName = "Tom";
+    std::cout << "strName : " << strName << ", "
+              << typeid(strName).name() << std::endl;
+
+    auto ch = 'A';
+    std::cout << "ch : " << ch << ", "
+              << typeid(ch).name() << std::endl;
+    
+    
 
     return 0;
 }
