a3c199d2972c9164a4eeb7162a3ae828cbafe65b
sian7563 <sian7563@naver.com>
2021-08-25 09:11:23 +0900

    typeid 예제

--- c++2/c++2/main.cpp
+++ c++2/c++2/main.cpp
@@ -31,22 +31,30 @@ int main()
 //    std::cout << "ch : " << ch << ", "
 //              << typeid(ch).name() << std::endl;
     
-    auto a = true;
-    auto b = 'A';
-    auto c = L'B';
-    auto d = "문자열";
-    auto e = 815;
-    auto f = 3.14;
+//    auto a = true;
+//    auto b = 'A';
+//    auto c = L'B';
+//    auto d = "문자열";
+//    auto e = 815;
+//    auto f = 3.14;
+//
+//    std::cout << "값\t크기\t자료형" << std::endl;
+//    std::cout << a << '\t' << sizeof(a) << '\t' << typeid(a).name() << std::endl;
+//    std::cout << b << '\t' << sizeof(b) << '\t' << typeid(b).name() << std::endl;
+//    std::cout << c << '\t' << sizeof(c) << '\t' << typeid(c).name() << std::endl;
+//    std::cout << d << '\t' << sizeof(d) << '\t' << typeid(d).name() << std::endl;
+//    std::cout << e << '\t' << sizeof(e) << '\t' << typeid(e).name() << std::endl;
+//    std::cout << f << '\t' << sizeof(f) << '\t' << typeid(f).name() << std::endl;
+    
+    
+    int nums[] = { 1, 2, 3 };
 
-    std::cout << "값\t크기\t자료형" << std::endl;
-    std::cout << a << '\t' << sizeof(a) << '\t' << typeid(a).name() << std::endl;
-    std::cout << b << '\t' << sizeof(b) << '\t' << typeid(b).name() << std::endl;
-    std::cout << c << '\t' << sizeof(c) << '\t' << typeid(c).name() << std::endl;
-    std::cout << d << '\t' << sizeof(d) << '\t' << typeid(d).name() << std::endl;
-    std::cout << e << '\t' << sizeof(e) << '\t' << typeid(e).name() << std::endl;
-    std::cout << f << '\t' << sizeof(f) << '\t' << typeid(f).name() << std::endl;
-    
-    
+    std::cout << typeid(1).name() << std::endl;
+    std::cout << typeid(10 + 3.2).name() << std::endl;
+    std::cout << typeid(nums).name() << std::endl;
+    std::cout << typeid(&nums).name() << std::endl;
+    std::cout << typeid(&nums[0]).name() << std::endl;
+
     
 
     return 0;
